0a2a3b0943c2463ca38b673c6c42d2e3e9bc2d5a
Jostein Kjønigsen <jostein@kjonigsen.net>
2015-01-25 16:08:32 +0100

    Fix Emacs-lockup during fontification.
    Fixes non-exiting loop in detection of square-parentasis regions.
    
    Closes https://github.com/josteink/csharp-mode/issues/17.

--- csharp-mode.el
+++ csharp-mode.el
@@ -539,6 +539,20 @@ comment at the start of cc-engine.el for more info."
       rtn))
 
 
+(defun csharp-is-square-parentasis-block-p ()
+  "Attempts to safely assess if the current point is at the opening of
+a square parentasis block [ ... ]."
+  (let* ((start (point)) ;; variables used to hold our position, so that we know that
+	 (end))          ;; our code isn't stuck trying to look for a non-existant sexp.
+    (and (eq (char-after) 91) ;; open square
+	 (while (and (eq (char-after) 91)
+		     (not (eq start end)))
+	   (c-safe (c-forward-sexp 1))
+	   (setq end (point)))
+	 (eq (char-before) 93))) ;; close square
+  )
+
+
 
 ;; ==================================================================
 ;; end of csharp-mode utility and feature defuns
@@ -942,10 +956,7 @@ comment at the start of cc-engine.el for more info."
 
                                 (if (or
                                      (eq (char-after) ?{) ;; open curly
-                                     (and (eq (char-after) 91) ;; open square
-                                          (while (eq (char-after) 91)
-                                            (c-safe (c-forward-sexp 1)))
-                                          (eq (char-before) 93)) ;; close square
+                                     (csharp-is-square-parentasis-block-p)
                                      (and (eq (char-after) 40) ;; open paren
                                           (c-safe (c-forward-sexp 1) t)))
 
