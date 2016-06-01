b609f424c8b9d8febf163ed08a82dd3127adeef5
Jostein Kjønigsen <jostein@kjonigsen.net>
2016-06-01 14:49:05 +0200

    New imenu test-file for method-indexing.
    Has lots of failing test-cases.
    
    Based on the following documentation:
    https://msdn.microsoft.com/en-us/library/6tcf2h8w.aspx

---
+++ test-files/imenu-method-test.cs
@@ -0,0 +1,33 @@
+using System;
+
+
+/// <summary>
+///   This class is used to test implementation of method-indexing.
+/// </summary>
+public class MethodTest
+{
+    [DllImport("user32.dll")]
+    public extern int GetTickCount64();
+
+    public async string OpenWebServiceAsync(string url)
+    {
+        await foo;
+    }
+
+    public new string GetString()
+    {
+
+    }
+
+    public virtual override bool Equals(object other)
+    {
+
+    }
+
+    public abstract string SubClassMethod();
+
+    public unsafe static void FastCopy(byte[] src, byte[] dst, int count)
+    {
+
+    }
+}
