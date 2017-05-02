1a009242ddb4ee9f6ee1e1528f11786a11c135e8
Tim Van Holder <tim.vanholder@leeloo.anubex.internal>
2017-05-02 10:31:17 +0200

    Added testcases for region fontification.
    Extended test-files/region-fontification.cs with more cases.
    Added a test to ensure all cases get fontified correctly.

--- test-files/region-fontification.cs
+++ test-files/region-fontification.cs
@@ -4,11 +4,40 @@ public class Test
 {
     public void Test()
     {
-        #region fontifies correctly
+        #region fontifies as a region comment
         string foo = "bar";
         #endregion
         
-        #region does'nt fontify correctly
+        #region quotes shouldn't mess up a region comment
+        string foo = "bar";
+        #endregion
+        
+        #region   any number of spaces are allowed before a region comment
+        string foo = "bar";
+        #endregion
+        
+        #region	a TAB is also allowed before a region comment
+        string foo = "bar";
+        #endregion
+
+        #  region a region comment is fine when there are spaces between the # and the region
+        string foo = "bar";
+        #  endregion
+        
+        #	region a TAB between the # and the region doesn't stop this being a region comment either
+        string foo = "bar";
+        #	endregion
+
+        #region --- a region comment doesn't have to start with a word ---
+        string foo = "bar";
+        #endregion
+
+        #region "a region comment can even look like this"
+        string foo = "bar";
+        #endregion
+
+        // This is here just to ensure the commentification doesn't bleed to the next line
+        #region
         string foo = "bar";
         #endregion
     }
