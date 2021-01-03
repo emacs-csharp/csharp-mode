ea465a98ce7637c08111c1a664d100a4b6116bd7
JenChieh <jcs090218@gmail.com>
2021-01-04 03:39:40 +0800

    Add commentary.

--- csharp-tree-sitter.el
+++ csharp-tree-sitter.el
@@ -23,6 +23,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+;;
+;; Tree Sitter support for C# mode.
+;;
 
 ;;; Code:
 (require 'cl-lib)
