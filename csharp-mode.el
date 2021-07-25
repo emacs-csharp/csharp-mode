69fe52b5908e1b29b2765d7e01cd6a8f0fc16332
Theodor Thornhill <theo@thornhill.no>
2021-07-25 20:41:59 +0200

    Version bump

--- csharp-mode.el
+++ csharp-mode.el
@@ -5,7 +5,7 @@
 ;;            : Theodor Thornhill <theo@thornhill.no>
 ;; Created    : September 2020
 ;; Modified   : 2020
-;; Version    : 0.11.0
+;; Version    : 0.12.0
 ;; Keywords   : c# languages oop mode
 ;; X-URL      : https://github.com/emacs-csharp/csharp-mode
 ;; Package-Requires: ((emacs "26.1"))
